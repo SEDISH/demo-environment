d540ef8e96691700e0dbe0832923bfdc5bb19420
Arkadiusz Lalo <alalo@soldevelo.com>
2017-08-25 15:39:30 +0200

    * Fixed issue with generate-cert script

--- scripts/generate-cert.sh
+++ scripts/generate-cert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p /etc/ssl/crt
-openssl req  -nodes -new -x509  -keyout /etc/ssl/crt/server.key -out /etc/ssl/crt/server.cert
+openssl req  -nodes -new -x509  -keyout /etc/ssl/crt/server.key -out /etc/ssl/crt/server.crt
