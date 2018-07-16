81026dda27b6a901bdd7b540906d2a0f36cc3238
Arkadiusz Lalo <alalo@soldevelo.com>
2018-07-16 14:03:48 +0200

    Fixed issue with data migration

--- scripts/data-migration/dropTmpTables.sql
+++ scripts/data-migration/dropTmpTables.sql
@@ -3,32 +3,32 @@ DROP PROCEDURE IF EXISTS dropTmpTables$$
 CREATE PROCEDURE dropTmpTables()
 BEGIN
 
-  DROP TABLE tmp_person;
-  DROP TABLE tmp_person_to_merge;
-  DROP TABLE tmp_person_attribute;
-  DROP TABLE tmp_person_attribute_type;
-  DROP TABLE tmp_patient_id;
-  DROP TABLE tmp_patient_id_type;
-  DROP TABLE tmp_person_name;
-  DROP TABLE tmp_person_address;
+  DROP TABLE IF EXISTS tmp_person;
+  DROP TABLE IF EXISTS tmp_person_to_merge;
+  DROP TABLE IF EXISTS tmp_person_attribute;
+  DROP TABLE IF EXISTS tmp_person_attribute_type;
+  DROP TABLE IF EXISTS tmp_patient_id;
+  DROP TABLE IF EXISTS tmp_patient_id_type;
+  DROP TABLE IF EXISTS tmp_person_name;
+  DROP TABLE IF EXISTS tmp_person_address;
 
-  DROP TABLE tmp_location;
-  DROP TABLE tmp_location_attribute;
-  DROP TABLE tmp_location_attribute_type;
-  DROP TABLE tmp_visit;
-  DROP TABLE tmp_visit_type;
+  DROP TABLE IF EXISTS tmp_location;
+  DROP TABLE IF EXISTS tmp_location_attribute;
+  DROP TABLE IF EXISTS tmp_location_attribute_type;
+  DROP TABLE IF EXISTS tmp_visit;
+  DROP TABLE IF EXISTS tmp_visit_type;
 
-  DROP TABLE tmp_encounter;
-  DROP TABLE tmp_encounter_type;
-  DROP TABLE tmp_privilege;
+  DROP TABLE IF EXISTS tmp_encounter;
+  DROP TABLE IF EXISTS tmp_encounter_type;
+  DROP TABLE IF EXISTS tmp_privilege;
 
-  DROP TABLE tmp_provider;
-  DROP TABLE tmp_provider_attribute;
-  DROP TABLE tmp_provider_attribute_type;
-  DROP TABLE tmp_encounter_role;
-  DROP TABLE tmp_encounter_provider;
+  DROP TABLE IF EXISTS tmp_provider;
+  DROP TABLE IF EXISTS tmp_provider_attribute;
+  DROP TABLE IF EXISTS tmp_provider_attribute_type;
+  DROP TABLE IF EXISTS tmp_encounter_role;
+  DROP TABLE IF EXISTS tmp_encounter_provider;
 
-  DROP TABLE tmp_obs;
+  DROP TABLE IF EXISTS tmp_obs;
 
 END $$
 DELIMITER ;
